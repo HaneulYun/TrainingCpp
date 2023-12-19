d097f40f7c8c0f338934a97aa3ad32a1d24c880a
HaneulYun <yhnil.kr@gmail.com>
2023-12-20 08:51:24 +0900

    [PCCP 기출문제] 1번
    https://school.programmers.co.kr/learn/courses/30/lessons/250137

--- TrainingCpp/solution.cpp
+++ TrainingCpp/solution.cpp
@@ -1,15 +1,34 @@
-#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main(void) {
-    string msg = "Spring is beginning";
-    int val1 = 3;
-    string val2 = "3";
+int solution(vector<int> bandage, int health, vector<vector<int>> attacks) {
+    int bandageTime = bandage[0];
+    int bandageHeal = bandage[1];
+    int bandageExtra = bandage[2];
 
-    cout << msg << endl;
-    cout << val1 + 10 << endl;
-    cout << val2 + "10" << endl;
+    int maxHealth = health;
 
+    int prevAttackTime = 0;
+    for (const auto& attack : attacks)
+    {
+        int time = attack[0] - prevAttackTime - 1;
+        health += time * bandageHeal;
+        health += (time / bandageTime) * bandageExtra;
+        if (health > maxHealth)
+            health = maxHealth;
+
+        prevAttackTime = attack[0];
+
+        health -= attack[1];
+        if (health <= 0)
+            return -1;
+    }
+    return health;
+}
+
+int main()
+{
     return 0;
 }
