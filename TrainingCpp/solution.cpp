813f2872ce9c2eaf4cf8f2ad7bfca36db8a9a8a2
HaneulYun <yhnil.kr@gmail.com>
2023-12-26 18:11:39 +0900

    폰켓몬
    https://school.programmers.co.kr/learn/courses/30/lessons/1845

--- TrainingCpp/solution.cpp
+++ TrainingCpp/solution.cpp
@@ -1,19 +1,12 @@
-#include <string>
 #include <vector>
-
+#include <set>
+#include <algorithm>
 using namespace std;
 
-int solution(vector<vector<string>> board, int h, int w) {
-    string c = board[h][w];
-    int l = board.size();
-    int s = 0;
-
-    if (w > 0 && board[h][w - 1] == c) ++s;
-    if (h > 0 && board[h - 1][w] == c) ++s;
-    if (w < l - 1 && board[h][w + 1] == c) ++s;
-    if (h < l - 1 && board[h + 1][w] == c) ++s;
-
-    return s;
+int solution(vector<int> nums)
+{
+    set<int> s{ nums.begin(), nums.end() };
+    return min(nums.size() / 2, s.size());
 }
 
 int main()
