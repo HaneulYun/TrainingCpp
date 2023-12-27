2d013bb624da62f3babb48aa524f713e285f52f0
HaneulYun <yhnil.kr@gmail.com>
2023-12-27 09:33:10 +0900

    [PCCE 기출문제] 4번 / 저축
    https://school.programmers.co.kr/learn/courses/30/lessons/250130

--- TrainingCpp/solution.cpp
+++ TrainingCpp/solution.cpp
@@ -3,17 +3,22 @@
 using namespace std;
 
 int main(void) {
-    int year, answer;
-    string age_type;
-    cin >> year >> age_type;
+    int start;
+    int before;
+    int after;
+    cin >> start >> before >> after;
 
-    if (age_type == "Korea") {
-        answer = 2030 - year + 1;
-    }
-    else if (age_type == "Year") { 
-        answer = 2030 - year;
-    }
+    int money = start;
+    int month = 1;
 
-    cout << answer << endl;
+    while (money < 70) {
+        money += before;
+        month++;
+    }
+    while (money < 100) {
+        money += after;
+        month++;
+    }
+    cout << month << endl;
     return 0;
 }
