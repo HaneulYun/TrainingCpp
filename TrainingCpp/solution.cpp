a21e29c5fc5118259f9c856bd1f3830c1b49efd4
HaneulYun <yhnil.kr@gmail.com>
2023-12-28 15:43:06 +0900

    달리기 경주
    https://school.programmers.co.kr/learn/courses/30/lessons/178871

--- TrainingCpp/solution.cpp
+++ TrainingCpp/solution.cpp
@@ -1,14 +1,22 @@
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
-vector<int> solution(int brown, int yellow) {
-    brown = (brown - 4) / 2;
-    for (int i = 0; i < brown; ++i)
-        if (i * (brown - i) == yellow)
-            return vector<int>{brown - i + 2, i + 2};
-    return {};
+vector<string> solution(vector<string> players, vector<string> callings) {
+    map<string, int> pos;
+    for (int i = 0; i < players.size(); ++i)
+        pos[players[i]] = i;
+
+    for (const auto& calling : callings)
+    {
+        int p = pos[calling];
+        swap(players.at(p - 1), players.at(p));
+        swap(pos[players.at(p - 1)], pos[players.at(p)]);
+    }
+
+    return players;
 }
 
 int main()
