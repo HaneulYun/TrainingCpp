7e85e29c18a72b2d2eecb27e144df28e35169564
HaneulYun <yhnil.kr@gmail.com>
2023-12-31 06:34:48 +0900

    [PCCE 기출문제] 10번 / 데이터 분석
    https://school.programmers.co.kr/learn/courses/30/lessons/250121

--- TrainingCpp/solution.cpp
+++ TrainingCpp/solution.cpp
@@ -1,22 +1,20 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-vector<string> solution(vector<int> numbers, vector<int> our_score, vector<int> score_list) {
-    int num_student = numbers.size();
-    vector<string> answer(num_student);
-
-    for (int i = 0; i < num_student; i++) {
-        if (our_score[i] == score_list[numbers[i] - 1]) {
-            answer[i] = "Same";
-        }
-        else {
-            answer[i] = "Different";
-        }
-    }
-
-    return answer;
+vector<vector<int>> solution(vector<vector<int>> data, string ext, int val_ext, string sort_by) {
+    vector<string> exts{ "code", "date", "maximum", "remain" };
+    int ext_idx = find(exts.begin(), exts.end(), ext) - exts.begin();
+    data.erase(remove_if(data.begin(), data.end(), [&](const vector<int>& v) {
+        return v[ext_idx] > val_ext;
+        }), data.end());
+    int sort_by_idx = find(exts.begin(), exts.end(), sort_by) - exts.begin();
+    sort(data.begin(), data.end(), [&](const auto& l, const auto& r) {
+        return l[sort_by_idx] < r[sort_by_idx];
+        });
+    return data;
 }
 
 int main()
