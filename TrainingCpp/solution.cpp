6b431f6b212a5689b1dbba1413f07273490b9c0a
HaneulYun <yhnil.kr@gmail.com>
2023-12-31 06:35:17 +0900

    두 원 사이의 정수 쌍
    https://school.programmers.co.kr/learn/courses/30/lessons/181187

--- TrainingCpp/solution.cpp
+++ TrainingCpp/solution.cpp
@@ -1,20 +1,15 @@
-#include <string>
-#include <vector>
-#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
-vector<vector<int>> solution(vector<vector<int>> data, string ext, int val_ext, string sort_by) {
-    vector<string> exts{ "code", "date", "maximum", "remain" };
-    int ext_idx = find(exts.begin(), exts.end(), ext) - exts.begin();
-    data.erase(remove_if(data.begin(), data.end(), [&](const vector<int>& v) {
-        return v[ext_idx] > val_ext;
-        }), data.end());
-    int sort_by_idx = find(exts.begin(), exts.end(), sort_by) - exts.begin();
-    sort(data.begin(), data.end(), [&](const auto& l, const auto& r) {
-        return l[sort_by_idx] < r[sort_by_idx];
-        });
-    return data;
+long long solution(int r1, int r2) {
+    long long answer = 0;
+    for (long long i = 0; i < r2; ++i)
+        answer += sqrtl((long long)r2 * r2 - i * i);
+    for (long long i = 0; i < r1; ++i)
+        answer -= ceil(sqrtl((long long)r1 * r1 - i * i)) - 1;
+    answer *= 4;
+    return answer;
 }
 
 int main()
