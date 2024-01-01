652da271a2805d05c1424f74a6267c92e1ba426d
HaneulYun <yhnil.kr@gmail.com>
2024-01-01 12:16:23 +0900

    최빈값 구하기
    https://school.programmers.co.kr/learn/courses/30/lessons/120812

--- TrainingCpp/solution.cpp
+++ TrainingCpp/solution.cpp
@@ -1,12 +1,18 @@
+#include <algorithm>
 #include <vector>
+#include <map>
 
 using namespace std;
 
-int solution(vector<int> num_list) {
-    int a = 0, b = 0;
-    for (auto n : num_list)
-        ((n % 2 ? a : b) *= 10) += n;
-    return a + b;
+int solution(vector<int> array) {
+    int answer = 0;
+    map<int, int> m;
+    for (const auto n : array)
+        ++m[n];
+    auto pred = [](const auto& l, const auto& r) { return l.second < r.second; };
+    auto gm = max_element(m.begin(), m.end(), pred);
+    auto rm = max_element(m.rbegin(), m.rend(), pred);
+    return gm->first == rm->first ? gm->first : -1;
 }
 
 int main()
